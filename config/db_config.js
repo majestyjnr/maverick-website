fa567da895e615097bf70565a97cd6759dafe475
Dr. Aidoo <aidoojuniorsolomon@gmail.com>
2020-11-06 23:23:30 -0800

    New Commit

--- config/db_config.js
+++ config/db_config.js
@@ -1,6 +1,6 @@
 module.exports = {
   MongoURI:
-    "mongodb+srv://Majesty:<Majesty1@>@maverickdb.plvuq.mongodb.net/<MajestyDB>?retryWrites=true&w=majority",
+    "mongodb+srv://Majesty:Majesty1@@maverickdb.plvuq.mongodb.net/MajestyDB?retryWrites=true&w=majority",
 };
 
 // MongoURI : "mongodb://localhost:27017/maverickDB"
