0b798295f635a7eee1a89e6cf600315178271179
majestyjnr <aidoojuniorsolomon@gmail.com>
2021-02-23 03:00:47 -0800

    Commit

--- config/db_config.js
+++ config/db_config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  MongoURI: "mongodb+srv://Majesty:Majesty1@@maverickdb.plvuq.mongodb.net/MajestyDB?retryWrites=true&w=majority",
+  MongoURI: "mongodb://localhost:27017/maverickDB",
 };
+
+// "mongodb+srv://Majesty:Majesty1@@maverickdb.plvuq.mongodb.net/MajestyDB?retryWrites=true&w=majority",
 
 // "mongodb://localhost:27017/maverickDB",
